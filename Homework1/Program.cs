56b3914ea53cfae9ea01e67524b0429349af6c19
ivankopanyov <i.kopanyov@gmail.com>
2022-05-17 11:53:21 +0300

    Update Homework1

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -6,6 +6,15 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
+            // Просим пользователя ввести имя и сохраняем результат в переменную
+            Console.Write("Введите ваше имя: ");
+            string userName = Console.ReadLine();
+
+            //Выводим в консоль сообщение с именем пользователя и текущей датой
+            Console.WriteLine($"Привет, {userName}, сегодня {DateTime.Now}");
+
+            //Выходим из приложения по нажатию клавиши
+            Console.ReadKey();
         }
     }
 }
