ba8fb4a8c75e1cc6a6d77914225daf39bd4c87c8
ivankopanyov <i.kopanyov@gmail.com>
2022-05-19 12:10:04 +0300

    * Homework.sln: Create Homework3
    * Program.cs:
    * SeaBattle.cs:
    * Homework3.csproj:

---
+++ Homework3/Program.cs
@@ -0,0 +1,258 @@
+﻿using System;
+
+namespace Homework3
+{
+    class Program
+    {
+        /// <summary>
+        /// Точка входа в приложение.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            OutputMainMenu();
+        }
+
+        /// <summary>
+        /// Вывод главного меню.
+        /// </summary>
+        static void OutputMainMenu()
+        {
+            Console.WriteLine("================ Домашняя работа №3 ================");
+            Console.WriteLine("1 - Вывод элементов двумерного массива по диагонали.");
+            Console.WriteLine("2 - Вывод телефонного справочника.");
+            Console.WriteLine("3 - Разворот строки.");
+            Console.WriteLine("4 - Вывод игрового поля для морского боя.");
+            Console.WriteLine();
+            Console.WriteLine("0 - Выход.");
+            Console.WriteLine("====================================================");
+            Console.WriteLine();
+
+            int number;
+            Console.Write("Укажите номер пункта меню: ");
+            if (!int.TryParse(Console.ReadLine(), out number))
+            {
+                number = -1;
+            }
+            Console.WriteLine();
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("\n-- Вывод элементов двумерного массива по диагонали --\n");
+                    Task1();
+                    break;
+                case 2:
+                    Console.WriteLine("\n----------- Вывод телефонного справочника -----------\n");
+                    Task2();
+                    break;
+                case 3:
+                    Console.WriteLine("\n------------------ Разворот строки ------------------\n");
+                    Task3();
+                    break;
+                case 4:
+                    Console.WriteLine("\n-------- Вывод игрового поля для морского боя -------\n");
+                    Task4();
+                    break;
+                case 0:
+                    Console.WriteLine("\nЗавершение работы приложения...\nНажмите любую клавишу");
+                    Console.ReadKey();
+                    return;
+                default:
+                    Console.WriteLine("\nНекорректный ввод.\nПовторите попытку.\n");
+                    OutputMainMenu();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Вывод подменю.
+        /// </summary>
+        /// <param name="taskNumber">Номер задачи.</param>
+        static void OutputSubMenu(int taskNumber)
+        {
+            Console.WriteLine();
+            Console.WriteLine("=============================");
+            Console.WriteLine("1 - Повторить.");
+            Console.WriteLine();
+            Console.WriteLine("0 - Выход в главное меню.");
+            Console.WriteLine("=============================");
+            Console.WriteLine();
+
+            int number;
+            Console.Write("Укажите номер пункта меню: ");
+            if (!int.TryParse(Console.ReadLine(), out number))
+            {
+                number = -1;
+            }
+            Console.WriteLine();
+
+            switch (number)
+            {
+                case 1:
+                    RepeatTask(taskNumber);
+                    break;
+                case 0:
+                    OutputMainMenu();
+                    break;
+                default:
+                    Console.WriteLine("Некорректный ввод.\nПовторите попытку.");
+                    OutputSubMenu(taskNumber);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Повтор текущей задачи.
+        /// </summary>
+        /// <param name="taskNumber">Номер задачи.</param>
+        static void RepeatTask(int taskNumber)
+        {
+            switch (taskNumber)
+            {
+                case 1:
+                    Task1();
+                    break;
+                case 2:
+                    Task2();
+                    break;
+                case 3:
+                    Task3();
+                    break;
+                case 4:
+                    Task4();
+                    break;
+                default:
+                    OutputMainMenu();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Задача 1. Вывод элементов двумерного массива по диагонали.
+        /// </summary>
+        static void Task1()
+        {
+            int length;
+            Console.Write("Укажите размерность массива (минимум 2): ");
+            if (!int.TryParse(Console.ReadLine(), out length) || length < 2)
+            {
+                Console.WriteLine("Некорректный ввод.\nПовторите попытку.\n");
+                Task1();
+                return;
+            }
+
+            int[,] array = new int[length, length];
+            Random random = new Random();
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(0, 10);
+                    if (i == j || j == length - (i + 1))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    Console.Write($"{array[i, j]} ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+
+            OutputSubMenu(1);
+        }
+
+        /// <summary>
+        /// Задача 2. Вывод телефонного справочника.
+        /// </summary>
+        static void Task2()
+        {
+            string[,] directory = new string[5, 2] {
+                { "Мама",    "+7 (900) 000-00-00 / mom@family.com" },
+                { "Папа",    "+7 (900) 111-11-11 / dad@family.com" },
+                { "Дедушка", "+7 (900) 222-22-22 / grandma@family.com" },
+                { "Бабушка", "+7 (900) 333-33-33 / grandpa@family.com" },
+                { "Брат",    "+7 (900) 444-44-44 / brother@family.com" }
+            };
+
+            int[] lengths = GetMaxLengths(directory);
+
+            for (int i = 0; i < directory.GetLength(0); i++)
+            {
+                for (int j = 0; j < directory.GetLength(1); j++)
+                {
+                    string str = directory[i, j];
+                    Console.Write($"{str} ");
+                    if (j > 0)
+                    {
+                        continue;
+                    }
+                    for (int k = 0; k < lengths[j] - str.Length; k++)
+                    {
+                        Console.Write(".");
+                    }
+                    Console.Write("... ");
+                }
+                Console.WriteLine();
+            }
+            OutputSubMenu(2);
+        }
+
+        /// <summary>
+        /// Поиск максимальной длины элемента в каждом измерении двумерного массива строк.
+        /// </summary>
+        /// <param name="array">Двумерный массив строк для вычисления максимальных длин элементов.</param>
+        /// <returns>Массив максимальныч длин элементов в каждом измерении двумерного массива строк.</returns>
+        static int[] GetMaxLengths(string[,] array)
+        {
+            int[] res = new int[array.GetLength(1)];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    int length = array[i, j].Length;
+                    if (length > res[j])
+                    {
+                        res[j] = length;
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Задача 3. Разворот строки.
+        /// </summary>
+        static void Task3()
+        {
+            Console.Write("Введите строку для разворота: ");
+            string str = Console.ReadLine();
+
+            if (str.Trim() == "")
+            {
+                Console.WriteLine("Строка не может быть пустой!\nПовторите попытку\n");
+                Task3();
+                return;
+            }
+
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            Console.WriteLine($"Результат: {new string(charArray)}");
+            OutputSubMenu(3);
+        }
+
+        /// <summary>
+        /// Задача 4. Вывод игрового поля для морского боя.
+        /// </summary>
+        static void Task4()
+        {
+            SeaBattle.DrawField();
+            OutputSubMenu(4);
+        }
+    }
+}
---
+++ Homework3/SeaBattle.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+namespace Homework3
+{
+    /// <summary>
+    /// Морской бой.
+    /// </summary>
+    public static class SeaBattle
+    {
+        /// <summary>
+        /// Длина стороны игрового поля по осям X и Y.
+        /// </summary>
+        private const int lengthSide = 10;
+
+        /// <summary>
+        /// Массив, содержащий результат заполнения клеток игрового поля.
+        /// </summary>
+        private static int[,] field;
+
+        /// <summary>
+        /// Создание и заполнение игрового поля.
+        /// </summary>
+        private static void CreateField()
+        {
+            int currentDecksNumber = 4;
+            int typesShipsNumber = 4;
+            Random random = new Random();
+
+            for (int shipsNumber = 1; shipsNumber <= typesShipsNumber; shipsNumber++)
+            {
+                for (int i = 0; i < shipsNumber; i++)
+                {
+                    bool check = true;
+                    int x, y, axis, direction;
+
+                    do
+                    {
+                        x = random.Next(0, lengthSide);
+                        y = random.Next(0, lengthSide);
+                        axis = random.Next(0, 2);
+                        direction = random.Next(0, 2) == 1 ? 1 : -1;
+                        int tempX = x;
+                        int tempY = y;
+
+                        for (int j = 0; j < currentDecksNumber; j++)
+                        {
+                            check = CheckCell(tempX, tempY);
+
+                            if (!check)
+                            {
+                                break;
+                            }
+
+                            if (axis == 0)
+                            {
+                                tempX += direction;
+                            }
+                            else
+                            {
+                                tempY += direction;
+                            }
+                        }
+                    }
+                    while (!check);
+
+                    for (int j = 0; j < currentDecksNumber; j++)
+                    {
+                        field[x, y] = 1;
+
+                        if (axis == 0)
+                        {
+                            x += direction;
+                        }
+                        else
+                        {
+                            y += direction;
+                        }
+                    }
+                }
+
+                currentDecksNumber--;
+            }
+        }
+
+        /// <summary>
+        /// Проверка клетки игрового поля.
+        /// </summary>
+        /// <param name="x">Координата клетки по оси X.</param>
+        /// <param name="y">Координата клетки по оси Y.</param>
+        /// <returns>Возвращает true, если клетка и клетки вокруг нее свободны.</returns>
+        private static bool CheckCell(int x, int y)
+        {
+            if (x < 0 || x >= lengthSide || y < 0 || y >= lengthSide || field[x, y] == 1)
+            {
+                return false;
+            }
+
+            (int, int)[] displacements = new (int, int)[] { (0, 1), (1, 1),
+                (1, 0), (1, -1), (0, -1), (-1, -1),  (-1, 0), (-1, 1) };
+
+            foreach (var displacement in displacements)
+            {
+                int disX = x + displacement.Item1;
+                int disY = y + displacement.Item2;
+
+                if (disX < 0 || disX >= lengthSide || disY < 0 || disY >= lengthSide)
+                {
+                    continue;
+                }
+
+                if (field[disX, disY] == 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Вывод игрового поля в консоль.
+        /// </summary>
+        public static void DrawField()
+        {
+            field = new int[lengthSide, lengthSide];
+
+            string abc = "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЭЮЯ";
+
+            CreateField();
+
+            Console.Write("\n    ");
+            for (int i = 0; i < lengthSide; i++)
+            {
+                Console.Write($"{abc[i]} ");
+            }
+            Console.WriteLine("\n    ___________________");
+
+            for (int y = 0; y < field.GetLength(1); y++)
+            {
+                Console.Write($"{y + 1} ");
+                if (y < 9)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+
+                for (int x = 0; x < field.GetLength(0); x++)
+                {
+                    string cell = field[x, y] == 1 ? "X" : ".";
+                    string separator = x == field.GetLength(0) - 1 ? "|" : " ";
+                    Console.Write($"{cell}{separator}");
+                }
+                Console.Write("\n");
+            }
+
+            Console.WriteLine("    ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
+        }
+    }
+}
